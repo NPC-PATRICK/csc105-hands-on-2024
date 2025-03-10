399e2e4e667d1c7c33ebdc6c81c0fb193bd1914f
NPC-PATRICK <patnpc1603@gmail.com>
2025-03-10 14:56:16 +0700

    sc  commit

---
+++ ShoppingList/src/component/List.jsx
@@ -0,0 +1,14 @@
+import react from 'react'
+
+function List(){
+    return(
+        
+        <div className="List-Container">
+            <div className="list">
+            <h1>Shopping List</h1>
+            </div>
+        </div>
+    );
+    //http://localhost:5173/
+}
+export default List;
